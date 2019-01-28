c4598f9aa9ba156e64bb0a87e9ab1d6f8879edd4
Eugene <q010@bk.ru>
2019-01-28 21:17:40 +0300

    creating course model

---
+++ db/migrate/20190128181543_create_courses.rb
@@ -0,0 +1,11 @@
+class CreateCourses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.integer :duration
+      t.integer :difficulty
+      t.integer :views, default: 0
+      t.timestamps
+    end
+  end
+end
