04fe6465529247161124097841440a81f73e6f80
Eugene <q010@bk.ru>
2019-01-28 21:26:36 +0300

    creating ownership and membership models

---
+++ db/migrate/20190128181847_create_ownerships.rb
@@ -0,0 +1,9 @@
+class CreateOwnerships < ActiveRecord::Migration[5.2]
+  def change
+    create_table :ownerships do |t|
+      t.references :ownable, polymorphic: true, index: true
+      t.references :course, foreign_key: true
+      t.timestamps
+    end
+  end
+end
