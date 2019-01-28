8880f32e4b4baaaea80fc1ce73aab5adfeb1c5c9
Eugene <q010@bk.ru>
2019-01-28 21:14:32 +0300

    adding fields to org

--- db/migrate/20190128180422_create_organizations.rb
+++ db/migrate/20190128180422_create_organizations.rb
@@ -1,8 +1,11 @@
 class CreateOrganizations < ActiveRecord::Migration[5.2]
   def change
     create_table :organizations do |t|
-
+      t.string :name
+      t.text :description
       t.timestamps
     end
+
+    add_index :organizations, :name, unique: true
   end
 end
