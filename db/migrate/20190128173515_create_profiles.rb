84fb96c90c56f3a7788b300b468840d9bcd41ef2
Eugene <q010@bk.ru>
2019-01-28 20:43:42 +0300

    adding fields to profile

--- db/migrate/20190128173515_create_profiles.rb
+++ db/migrate/20190128173515_create_profiles.rb
@@ -1,8 +1,15 @@
 class CreateProfiles < ActiveRecord::Migration[5.2]
   def change
     create_table :profiles do |t|
-
+      t.references :user, foreign_key: true
+      t.string :name
+      t.string :surname
+      t.string :nickname
+      t.string :address
+      t.date :birthday
       t.timestamps
     end
+  
+    add_index :profiles, :nickname, unique: true
   end
 end
