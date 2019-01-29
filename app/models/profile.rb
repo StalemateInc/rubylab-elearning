b607b4eac41d7ff03bb172ff9765aa28b6ad50cd
Egor Dovnar <egordovnar.job@gmail.com>
2019-01-29 17:34:22 +0300

    Removed address validation, tweaked minimum nickname length a little.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,5 +3,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
   validates :name, :surname, presence: true, length: { in: 4..20 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :nickname, :address, presence: true, length: { in: 2..50 }
+  validates :nickname, presence: true, length: { in: 3..50 }
 end
