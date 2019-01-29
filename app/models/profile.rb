0eb98c8d674f1423d7e2c24c3cbc6bd0fe97e599
Alexander <alexandersazonof0@gmail.com>
2019-01-29 16:08:00 +0300

    add validation for profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,6 @@
 
 class Profile < ApplicationRecord
   belongs_to :user
+  validates :name, :surname, presence: true, length: { in: 4..20 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :nickname, :address, presence: true, length: { in: 2..50 }
 end
