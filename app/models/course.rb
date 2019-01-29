c8580c6623bd3d2bfd809a1851187c64ae3e578a
Alexander <alexandersazonof0@gmail.com>
2019-01-29 16:34:38 +0300

    add validation for course, feedback and organization models

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,4 +5,7 @@ class Course < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :passers, through: :participations, source: :user
   has_many :feedbacks
+
+  validates :name, presence: true, length: { in: 4..20 }
+  validates :duration, :views, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,4 +5,7 @@ class Organization < ApplicationRecord
   has_many :users, through: :memberships
   has_many :ownerships, as: :ownable
   has_many :created_courses, through: :ownerships, source: :course
+
+  validates :name, presence: true, length: { in: 2..20 }
+  validates :description, length: { maximum: 500 }
 end
