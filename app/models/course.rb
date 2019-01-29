51cd41efcb77aaf6a02d8a26bb08304859a1f9b5
Egor Dovnar <egordovnar.job@gmail.com>
2019-01-29 17:25:59 +0300

    Changed method namings on Course-User relationship. Added #owner method to Course.

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
+
   has_one :ownership, dependent: :destroy
   has_many :participations, dependent: :destroy
-  has_many :passers, through: :participations, source: :user
+  has_many :students, through: :participations, source: :user
   has_many :feedbacks
 
   validates :name, presence: true, length: { in: 4..20 }
   validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :views, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def owner
+    owner_record = self.ownership
+    owner_record.nil? ? owner_record : owner_record.ownable
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
   has_many :participations, dependent: :destroy
-  has_many :passing_courses, through: :participations, source: :course
+  has_many :enrolled_courses, through: :participations, source: :course
   has_many :feedbacks
 end
