8f17f4e58924237bfa81f219403e176e611868a9
Egor Dovnar <egordovnar.job@gmail.com>
2019-02-03 01:25:15 +0300

    Added HomeController with #index action. Generated devise controllers. Remapped devise routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # commented for starting purposes
-  # root to: 'home#index'
+  root to: 'home#index'
+  devise_for :users,
+             path: 'auth',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               password: 'secret',
+               confirmation: 'verification',
+               unlock: 'unblock',
+               registration: 'register',
+               sign_up: 'sign_up'
+             },
+             controllers: {
+               sessions: 'users/sessions',
+               # omniauth_callbacks: 'users/omniauth_callbacks',
+               unlocks: 'users/unlocks',
+               registrations: 'users/registrations',
+               passwords: 'users/passwords',
+               confirmations: 'users/confirmations'
+             }
 end
