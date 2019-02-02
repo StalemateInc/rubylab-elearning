7603a6a2d5a02e1524dd6bee8b495e373866fb64
Egor Dovnar <egordovnar.job@gmail.com>
2019-02-03 01:47:18 +0300

    Added required methods to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_one :profile, dependent: :destroy
@@ -13,4 +14,30 @@ class User < ApplicationRecord
   has_many :participations, dependent: :destroy
   has_many :enrolled_courses, through: :participations, source: :course
   has_many :feedbacks
+
+  def attempt_set_password(params)
+    p = {}
+    p[:password] = params[:password]
+    p[:password_confirmation] = params[:password_confirmation]
+    update_attributes(p)
+  end
+
+  # new function to return whether a password has been set
+  def has_no_password?
+    encrypted_password.blank?
+  end
+
+  # new function to provide access to protected method unless_confirmed
+  def only_if_unconfirmed
+    unless_confirmed { yield }
+  end
+
+  def password_required?
+    # Password is required if it is being set, but not for new records
+    if !persisted?
+      false
+    else
+      !password.nil? || !password_confirmation.nil?
+    end
+  end
 end
