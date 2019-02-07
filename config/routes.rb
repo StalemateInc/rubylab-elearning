ef4acc0bdd28e486376c71fa260f3592c681b3fa
Alexander <alexandersazonof0@gmail.com>
2019-02-07 16:38:01 +0300

    Edit route

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   root 'home#index'
 
   authenticate :user, lambda { |u| u.admin } do
-    mount Sidekiq::Web => '/sidekiq' 
-    
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   devise_for :users,
              path: 'auth',
              path_names: {
