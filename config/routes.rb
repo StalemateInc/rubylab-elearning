3f704da6036d066d21f5a97edf7d6dde45f3f986
Alexander <alexandersazonof0@gmail.com>
2019-02-07 22:22:15 +0300

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require 'sidekiq/web'
-Rails.application.routes.draw do]
+Rails.application.routes.draw do
   root 'home#index'
 
   authenticate :user, lambda { |u| u.admin } do
