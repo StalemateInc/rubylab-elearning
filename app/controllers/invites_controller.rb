51fb85335da3b29d57fdc87151871a36fd799b1a
Alexander <alexandersazonof0@gmail.com>
2019-02-08 21:49:38 +0300

    Rafectoring code

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,16 +3,16 @@
 class InvitesController < ApplicationController
   # before_action :set_invite, except: %i[accept]
 
-  def notificate; end
+  def index; end
 
   def accept
     if invite.nil?
       redirect_to root_path
     else
       @organization_id = invite.organization_id
-      @user_id = invite.user_id
-      Membership.create(organization_id: @organization_id, user_id: @user_id)
-      Invite.destroy(invite.id)
+      user_id = invite.user_id
+      Membership.create(organization_id: @organization_id, user_id: user_id)
+      invite.destroy
       redirect_to organization
     end
   end
@@ -21,7 +21,7 @@ class InvitesController < ApplicationController
     if invite.nil?
       redirect_to root_path
     else
-      Invite.destroy(invite.id)
+      invite.destroy
       redirect_to invites_path
     end
   end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,7 +2,4 @@ class Invite < ApplicationRecord
   belongs_to :user
   belongs_to :organization
 
-  def organization_name
-    Organization.find(organization_id).name
-  end
 end
