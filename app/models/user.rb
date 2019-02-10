e85355d7b4627752c7eabde2c2971c7915a82b12
Pavlukov <sergeybmzh@gmail.com>
2019-02-10 15:48:25 +0300

    Added allowed courses to users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
   has_many :certificates, through: :completion_records
   has_many :user_answers, dependent: :destroy
   has_many :course_accesses, dependent: :destroy
+  has_many :allowed_courses, through: :course_accesses, source: :course
   has_many :favorite_courses, dependent: :destroy
   has_many :impersonation_histories, class_name: 'ImpersonationHistory', foreign_key: :impersonator_id
 
