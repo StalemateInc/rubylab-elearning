eab3a8bf9aac3e25d61c3a9863f1ce0e037998f7
Eugene <q010@bk.ru>
2019-02-15 22:26:02 +0300

    added polymorfic function for adding new answers

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,29 +1,27 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     var answerIndex = 0;
-    document.getElementById('add-answer')
-                    .addEventListener('click', function () {
-                        var newAnswerBlock = document.createElement('div');
-                        newAnswerBlock.classList.add('answer');
-                        var newAnswer = document.createElement('input');
-                        var rightAnswer = document.createElement('input');
-                        newAnswer.setAttribute('name', 'answer[' + answerIndex + ']');
-                        rightAnswer.setAttribute('name', 'right-answer[]');
-                        rightAnswer.setAttribute('type', 'radio');
-                        rightAnswer.setAttribute('value', answerIndex);
-                        newAnswer.setAttribute('type', 'text');
-                        newAnswer.setAttribute('value', 'text of the answer');
-                        var answersContainer = document.querySelector('.answers');
-                        newAnswerBlock.appendChild(newAnswer);
-                        newAnswerBlock.appendChild(rightAnswer);
-                        answersContainer.appendChild(newAnswerBlock);
-                        answerIndex++;
+    var answerButtons = document.querySelectorAll('.add-answer');
+    answerButtons.forEach(function (answerButton) {
+        answerButton.addEventListener('click', function () {
+            addAnswer(answerButton.getAttribute('type'), answerIndex, answerButton.parentNode.querySelector('.answers'));
+            answerIndex++;
         });
+    });
 });
 
-function addAnswer(type) {
-    switch (type) {
-        case 1: break;
-        case 2: break;
-    }
+function addAnswer(type, answerIndex, answersContainer) {
+    var newAnswerBlock = document.createElement('div');
+    newAnswerBlock.classList.add('answer');
+    var newAnswer = document.createElement('input');
+    var rightAnswer = document.createElement('input');
+    newAnswer.setAttribute('name', 'answer[' + answerIndex + ']');
+    rightAnswer.setAttribute('name', 'right-answer[]');
+    rightAnswer.setAttribute('type', type);
+    rightAnswer.setAttribute('value', answerIndex);
+    newAnswer.setAttribute('type', 'text');
+    newAnswer.setAttribute('value', 'text of the answer');
+    newAnswerBlock.appendChild(newAnswer);
+    newAnswerBlock.appendChild(rightAnswer);
+    answersContainer.appendChild(newAnswerBlock);
 }
