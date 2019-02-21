56fdacf2dd133c737e33b149084b898724aaed84
Alexander <alexandersazonof0@gmail.com>
2019-02-21 10:49:02 +0300

    Add sort with problem

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -5,10 +5,11 @@ class OrganizationsController < ApplicationController
   before_action :authenticate_user!, except: %i[index show]
   before_action :set_organization, except: %i[index create new]
   before_action :set_join_request, only: :show
+  before_action :set_keyword, :get_organizations, only: :index
 
   # GET /organizations
   def index
-    @organizations = Organization.paginate(page: params[:page], per_page: 10)
+    @organizations
   end
 
   # POST /organizations
@@ -54,5 +55,40 @@ class OrganizationsController < ApplicationController
 
   def organization_params
     params.require(:organization).permit(%i[name description])
+  end
+
+  def get_organizations
+    case @keyword
+    when "wordsA"
+      @organizations = Organization.order(:name).paginate(page: params[:page], per_page: 10)
+    when "wordsZ"
+      @organizations = Organization.order(name: :desc).paginate(page: params[:page], per_page: 10)
+    when "course"
+      #select organizations.id, organizations.name, organizations.state, organizations.updated_at,
+      #organizations.created_at, organizations.description from organizations
+      #left join ownerships on ownerships.ownable_id = organizations.id
+      #group by organizations.id
+      #order by count(ownerships) desc ;
+    when "members"
+      #select organizations.id, organizations.name, organizations.state, organizations.updated_at,
+      #organizations.created_at, organizations.description from organizations
+      #join memberships on memberships.organization_id = organizations.id
+      #group by organizations.id
+      #order by count(memberships) desc ;
+    when "new"
+      @organizations = Organization.order(:created_at).paginate(page: params[:page], per_page: 10)
+    when "old"
+      @organizations = Organization.order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+    else
+      @organizations = Organization.all.paginate(page: params[:page], per_page: 10)
+    end
+  end
+
+  def set_keyword
+    @keyword = keyword_params
+  end
+
+  def keyword_params
+    params[:keyword].nil? ? nil : params.require(:keyword)
   end
 end
