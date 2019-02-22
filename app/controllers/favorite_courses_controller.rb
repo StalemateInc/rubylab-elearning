7ee2a887f6599ee507dd14f2eaf455571e259fa0
Pavlukov <sergeybmzh@gmail.com>
2019-02-22 21:16:21 +0300

    Added remove action for favorite courses.

--- app/controllers/favorite_courses_controller.rb
+++ app/controllers/favorite_courses_controller.rb
@@ -2,18 +2,28 @@ class FavoriteCoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_course
 
-  # POST /courses/:id/favorite
+  # POST /courses/:id/to_favorite
   def create
-    
-    binding.pry
-    
-    # @favorite_course = FavoriteCourse.new(course: @course, user: current_user)
-    # if @favorite_course.save
-    #   flash[:success] = 'Successfully added to favorite courses'
-    # else
-    #   flash[:notice] = 'An error occured while adding to favorite courses'
-    # end
-    # redirect_back(fallback_location: root_path)
+    @favorite_course = FavoriteCourse.new(course: @course, user: current_user)
+
+    if @favorite_course.save
+      flash[:success] = 'Successfully added to favorite courses'
+    else
+      flash[:notice] = 'An error occured while adding to favorite courses'
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  # DELETE /courses/:id/remove_favorite
+  def destroy
+    @favorite_course = FavoriteCourse.find_by(course: @course, user: current_user)
+
+    if @favorite_course.destroy
+      flash[:success] = 'Successfully removed from favorite courses'
+    else
+      flash[:notice] = 'An error occured while removing from favorite courses'
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   private
