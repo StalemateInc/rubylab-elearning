51e26c87bdb2b7744060ab6750d953e4ccddc060
Pavlukov <sergeybmzh@gmail.com>
2019-02-23 13:55:17 +0300

    Added favorite courses to user's dashboard.

--- app/controllers/user_dashboard_controller.rb
+++ app/controllers/user_dashboard_controller.rb
@@ -4,6 +4,8 @@ class UserDashboardController < ApplicationController
   before_action :authenticate_user!
 
   # GET /user
-  def index; end
+  def index
+    @favorite_courses = FavoriteCourse.where(user: current_user)
+  end
 
 end
