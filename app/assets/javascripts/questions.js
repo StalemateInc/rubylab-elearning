ad65070322daecbda237149371e945dc3a19fb3b
Eugene <q010@bk.ru>
2019-02-24 22:06:37 +0300

    indexed answers in each form

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -2,16 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var questionForm = document.getElementById('render_form');
     questionForm.addEventListener('ajax:success', function(event) {
         [data, status, xhr] = event.detail;
-        document.querySelector('.questions').appendChild(data.body.firstChild);
-    });
-    var answerIndex = 0;
-    var answerButtons = document.querySelectorAll('.add-answer');
-    answerButtons.forEach(function (answerButton) {
+        var newQuestionForm = data.body.firstChild;
+        document.querySelector('.questions').appendChild(newQuestionForm);
+        var answerButton = newQuestionForm.querySelector('.add-answer');
         answerButton.addEventListener('click', function () {
-            addAnswer(answerButton.getAttribute('type'), answerIndex, answerButton.parentNode.querySelector('.answers'));
-            answerIndex++;
+            addAnswer(answerButton.getAttribute('type'),
+                newQuestionForm.elements['index'].value,
+                answerButton.parentNode.querySelector('.answers'));
+            newQuestionForm.elements['index'].setAttribute('value',
+                parseInt(newQuestionForm.elements['index'].value) + 1);
         });
     });
+
 });
 
 function addAnswer(type, answerIndex, answersContainer) {
