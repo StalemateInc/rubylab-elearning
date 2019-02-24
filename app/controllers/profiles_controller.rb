31384170651dd7563bc36d7020449252765e573b
Pavlukov <sergeybmzh@gmail.com>
2019-02-24 17:25:33 +0300

    Added image uploader for user's profile.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,6 +12,7 @@ class ProfilesController < ApplicationController
 
   # PATCH /user/profile
   def update
+    @profile.remove_image!
     if @profile.update(profile_params)
       flash[:success] = 'You have successfully updated your profile'
     else
@@ -27,6 +28,6 @@ class ProfilesController < ApplicationController
   end
 
   def profile_params
-    params.require(:profile).permit(%i[name surname nickname address birthday])
+    params.require(:profile).permit(%i[name surname nickname address birthday image remove_image])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_135123) do
+ActiveRecord::Schema.define(version: 2019_02_24_141745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -182,6 +182,7 @@ ActiveRecord::Schema.define(version: 2019_02_24_135123) do
     t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["nickname"], name: "index_profiles_on_nickname", unique: true
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
