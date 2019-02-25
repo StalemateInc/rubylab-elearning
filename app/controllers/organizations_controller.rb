4215e123e7c3e5d5d2525829da06f3e0111da95e
Pavlukov <sergeybmzh@gmail.com>
2019-02-26 00:35:16 +0300

    Added fallback_url for organization images. Fixed bug with image removing.

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -33,7 +33,7 @@ class OrganizationsController < ApplicationController
   # PATCH /organizations/:id
   def update
     authorize @organization
-    @organization.remove_image!
+    @organization.remove_image! if organization_params[:remove_image] == 1
     redirect_to @organization if @organization.update(organization_params)
   end
 
