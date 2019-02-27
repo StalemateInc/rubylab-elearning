768513c96688551648ebc580f4b0d044e71487f0
Egor Dovnar <egordovnar.job@gmail.com>
2019-02-27 19:02:42 +0300

    Added CheckController for managing the check process for selected course. Added corresponding routes.

---
+++ app/controllers/check_controller.rb
@@ -0,0 +1,33 @@
+class CheckController < ApplicationController
+
+  before_action :set_course
+  before_action :set_user, except: :index
+
+  # GET /courses/:id/check/
+  def index
+    @awaiting_participations = @course.participations.where(await_check: true)
+  end
+
+  # GET /courses/:id/check/:user_id/
+  def show
+    @questions = @course.questions.includes(:user_answers)
+    @user_answers = @questions.map { |q| q.user_answers.where(user: @user) }
+    @zipped = @questions.zip(@user_answers)
+  end
+
+  # POST /courses/:id/check/:user_id/grade
+  def grade
+
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+end
