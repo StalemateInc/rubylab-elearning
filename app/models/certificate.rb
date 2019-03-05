de2aaae677534d263cc650da753b8dbec4e30ff7
Egor Dovnar <egordovnar.job@gmail.com>
2019-03-05 17:33:15 +0300

    Fixed bug in certificate generation. Added embed frame to certificate page to display certificate pdf.

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -2,5 +2,9 @@ class Certificate < ApplicationRecord
   belongs_to :completion_record
   belongs_to :course
 
-  after_destroy { |record| File.delete(Rails.root.join("/public/uploads/certificates/#{record.filename}")) }
+  after_destroy do |record|
+    full_filepath = Rails.root.join("/public/uploads/certificates/#{record.filename}")
+    File.delete(full_filepath) if File.exists?(full_filepath)
+  end
+
 end
