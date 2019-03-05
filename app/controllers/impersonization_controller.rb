b92a7f46608c6254e6a234e06014c76ede7e0113
Aleksandr Matskevich <saha.post@gmail.com>
2019-03-05 23:36:25 +0300

    final version of impersonization

--- app/controllers/impersonization_controller.rb
+++ app/controllers/impersonization_controller.rb
@@ -2,21 +2,25 @@ class ImpersonizationController < ApplicationController
   
 
   def impersonate
-    id = current_user.id
-    user = User.find(params[:id])
-    impersonate_user(user)
-    impersonation = ImpersonationHistory.create(started_at: Time.now, impersonator_id: id,
-      target_user_id: user.id)
-    redirect_to root_path
+    unless true_user && current_user && current_user != true_user
+      id = current_user.id
+      user = User.find(params[:id])
+      impersonate_user(user)
+      @impersonation = ImpersonationHistory.create(started_at: Time.now, impersonator_id: id,
+        target_user_id: user.id)
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   def stop_impersonating
+    cu = current_user.id
+    tu = true_user.id
     stop_impersonating_user
-    impersonation = ImpersonationHistory.find_by(impersonator_id: current_user.id,
-                                 target_user_id:  true_user.id,
+    impersonation = ImpersonationHistory.find_by(impersonator_id: tu,
+                                 target_user_id: cu,
                                  ended_at: nil)
     impersonation.update(ended_at: Time.now)
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 end
  
